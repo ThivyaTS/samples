9d252dc21538476f32a28691c5b1187319d3aca1
Thivya_TS <thivya0507@gmail.com>
2021-04-14 00:04:35 +0800

    First Commit for Location.java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,19 @@
+package samples.demo;
+/*public class location
+ * 
+ * @Thivya
+ */
+public class Location {
+	
+	private String name;
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+}
